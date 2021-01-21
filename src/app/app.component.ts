76cebae0def5f27340c605c394848edb755520f7
fjmartinez <fjmartinezduran@gmail.com>
2021-01-21 16:45:46 +0100

    Inicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import * as data from '../assets/users.json';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  <h1>Ejemplo de leer un JSON con Angular 11</h1>
+  <button (click)="firstWay()">Con TypeScript e <i>import Statement</i></button>
+  <br>
+  <br>
+  <button (click)="secondWay()">Con el <i>HttpClient</i></button>
+    `,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  
   title = 'readJson';
+
+  public contentfile:string='';
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  public firstWay():void{
+    console.log(data);    
+  }
+
+  public secondWay():void{
+    this.httpClient.get("assets/users.json").subscribe(data =>{
+      console.log(data);      
+    })
+  }
 }
